d92ee857dd59e0d5b558c3e7d2a1e48d8b6b4b00
Gurjinder <gurjindersr@gmail.com>
2019-01-22 05:23:46 +0530

    yet to be completed

--- webproject.js
+++ webproject.js
@@ -11,20 +11,89 @@ $(function(){
      attr:null,
  });
 });
-$(window).scroll(function() {
+// get initial position of the element
+//var fixmeTop = $('.introleft').offset().top;
+// $(window).scroll(function() {                  // assign scroll event listener
+//
+//     var currentScroll = $(this).scrollTop(); // get current position
+//
+//     if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+//         $('.introleft').fadeOut();
+//     } else {                                   // apply position: static
+//         $('.introleft').fadeIn();
+//     }
+//
+// });
 
-    if ($(this).scrollTop()>0)
-     {
-        $('.scroll').hide();
-        $('.introleft').fadeOut();
-        $('.introright').fadeOut();
-     }
-    else
-     {
+$(window).scroll(function() {
+    var introleftvar = $('.introleft').offset().top;
+    var scroll = this.scrollY;
+    var currentScroll = $(this).scrollTop();
+    if(scroll>=17500) {
+      $('.footerlefthide').fadeIn();
+      $('.heart').fadeIn();
+    }
+    else {
+      $('.footerlefthide').fadeOut();
+      $('.heart').fadeOut();
+    }
+    if(scroll>=7400 && scroll<=16200) {
+        $('.Iwantto').css({display:'block',position: 'fixed', top:'50%'});
+        if(scroll>=7600 && scroll<=9600) {
+          $('.sleep').fadeOut();
+          $('.sco').fadeOut();
+          $('.moon').fadeOut();
+        $('.sky').css({display:'block',position: 'fixed',margin:'10px' ,top:'50%',});
+        }
+        else if(scroll>=9800 && scroll<=11800) {
+          $('.sky').fadeOut();
+          $('.sleep').fadeOut();
+          $('.moon').fadeOut();
+        $('.sco').css({display:'block',position: 'fixed',margin:'10px' ,top:'50%',});
+        }
+        else if(scroll>=12000 && scroll<=14000) {
+          $('.sky').fadeOut();
+          $('.sco').fadeOut();
+          $('.sleep').fadeOut();
+        $('.moon').css({display:'block',position: 'fixed',margin:'10px' ,top:'50%',});
+        }
+        else if(scroll>=14200 && scroll<=16200) {
+          $('.sky').fadeOut();
+          $('.sco').fadeOut();
+          $('.moon').fadeOut();
+        $('.sleep').css({display:'block',position: 'fixed',margin:'10px' ,top:'50%',});
+        }
+        else {
+        $('.sleep').fadeOut();
+        $('.moon').fadeOut();
+        $('.sco').fadeOut();
+        $('.sky').fadeOut();
+        }
+    } else {
+        $('.sleep').fadeOut();
+        $('.moon').fadeOut();
+        $('.sco').fadeOut();
+        $('.sky').fadeOut();
+        $('.Iwantto').fadeOut();
+    }
+    if(currentScroll==introleftvar) {
       $('.introleft').fadeIn();
       if (window.innerWidth > 1100) {
         $('.introright').fadeIn();
         $('.scroll').fadeIn();
+      }
+    }
+    else if (currentScroll>introleftvar)
+     {
+        $('.scroll').hide();
+        $('.introleft').fadeOut();
+        $('.introright').fadeOut();
+        if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('.fixme').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '0',
+            left: '0'
+        });
       }
      }
  });
